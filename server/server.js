01c24b95a1d0c32c5ce4bf53661fc23d309e9c1d
mrmohit786 <32481317+mrmohit786@users.noreply.github.com>
2020-09-22 22:57:58 +0530

    Add server file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "..", "public");
 app.use(express.static(publicPath));
@@ -11,25 +10,3 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 app.listen(port, () => console.log("server is running!"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-]
